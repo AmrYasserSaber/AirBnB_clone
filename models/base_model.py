18dd96dedaf57cf190d19ed1350f53b03d8dc764
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-10-11 18:53:29 +0300

    base_model and its test

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.my_number = 89
+        self.name = "My First Model"
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        result = {
+            'my_number': self.my_number,
+            'name': self.name,
+            '__class__': self.__class__.__name__,
+            'updated_at': self.updated_at.isoformat(),
+            'id': self.id,
+            'created_at': self.created_at.isoformat()
+        }
+
+        return result
