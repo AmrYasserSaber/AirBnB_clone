0d276780b4659f0b6215436a75c1933407dfaa98
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-19 12:39:53 +0300

    Console.py

--- console.py
+++ console.py
@@ -1,1 +1,19 @@
-print("pass pycodeStyle")
+#!/usr/bin/env python3
+import cmd, sys
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+    doc_header = """
+    Documented commands (type help <topic>):
+    ========================================
+    EOF  help  quit
+
+    (hbnb)
+    """
+    def postcmd(self, stop, line):
+        print()
+    def do_exit(self, line):
+        'Exit'
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
