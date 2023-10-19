8ed5bd05cd860605bd2e4357152ab6d46c21390f
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-19 13:24:24 +0300

    Add Console.py File

--- console.py
+++ console.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-import cmd, sys
+import cmd
+import sys
+
+"""
+    implement of console file that manage all my action in this project
+"""
+
+
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     doc_header = """Documented commands (type help <topic>):"""
+
     def do_quit(self, line):
         "Quit command to exit the program"
         return True
+
     def do_EOF(self, line):
         "Quit command to exit the program"
         return True
+
     def postloop(self):
         print("")
 
