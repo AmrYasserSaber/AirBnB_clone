d554099806fc34e84357df682a9523e4bc6d1eba
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-19 16:49:18 +0300

    Add update method & fix pycodestyle issues

--- console.py
+++ console.py
@@ -13,7 +13,10 @@ import shlex
 
 
 my_models = {"BaseModel": BaseModel}
+
+
 class HBNBCommand(cmd.Cmd):
+
     prompt = "(hbnb) "
     doc_header = """Documented commands (type help <topic>):"""
 
@@ -32,6 +35,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         "over Write one empty line method"
         pass
+
     def do_create(self, model_name):
         "create an instance of a passed class"
         if not model_name:
@@ -50,8 +54,8 @@ class HBNBCommand(cmd.Cmd):
         class_name_id = shlex.split(args)
         # print(class_name_id)
         # print(all_model)
-    
-        if  len(class_name_id) == 0:
+
+        if len(class_name_id) == 0:
             print("** class name missing **")
         elif class_name_id[0] not in my_models.keys():
             print("** class doesn't exist **")
@@ -70,15 +74,17 @@ class HBNBCommand(cmd.Cmd):
                     break
             if check:
                 print("** no instance found **")
+
     def do_destroy(self, args):
-        "Deletes an instance based on the class name and id (save the change into the JSON file)."
+        """Deletes an instance based on the class name
+        and id (save the change into the JSON file)."""
         storage.reload()
         all_model = storage.all()
         class_name_id = shlex.split(args)
         # print(class_name_id)
         # print(all_model)
-    
-        if  len(class_name_id) == 0:
+
+        if len(class_name_id) == 0:
             print("** class name missing **")
         elif class_name_id[0] not in my_models.keys():
             print("** class doesn't exist **")
@@ -102,6 +108,7 @@ class HBNBCommand(cmd.Cmd):
         storage.reload()
         models = storage.all()
         check = False
+
         for key in models.keys():
             if model_name in key:
                 check = True
@@ -110,6 +117,33 @@ class HBNBCommand(cmd.Cmd):
                 print(models[key])
         else:
             print("** class doesn't exist **")
+
+    def do_update(self, args):
+        """Updates an instance based on the class
+        name and id by adding or updating attribute"""
+        storage.reload()
+        all_model = storage.all()
+        class_name_id = shlex.split(args)
+
+        if len(class_name_id) == 0:
+            print("** class name missing **")
+        elif class_name_id[0] not in all_model.keys():
+            print("** class doesn't exist **")
+        elif len(class_name_id) < 2:
+            print("** instance id missing **")
+        else:
+            curr_key = f"{class_name_id[0]}'>.{class_name_id[1]}"
+            check = True
+            for key in all_model.keys():
+                if curr_key in key:
+                    check = False
+            if check:
+                print("** no instance found **")
+            elif len(class_name_id) < 3:
+                print("** attribute name missing **")
+            elif len(class_name_id) < 4:
+                print("** value missing **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
