5f3b3a9d1a4b4a72f7a8f4bddf0b31ffe69df35b
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-10-19 14:37:06 +0300

    All models

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity Class"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = ""
---
+++ models/city.py
@@ -0,0 +1,7 @@
+from base_model import BaseModel
+
+
+class City(BaseModel):
+    """City Class"""
+    state_id = ""
+    name = ""
