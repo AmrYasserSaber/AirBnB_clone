5e035b27781fb37f818991118b3501798ad66413
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-10-20 23:12:28 +0300

    fix base model

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,10 @@ class BaseModel:
                 elif key != "__class__":
                     setattr(self, key, value)
 
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        models.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
