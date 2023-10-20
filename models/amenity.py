4c6427fc0c8c8cea179f6a83f05d0b9325a15724
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-20 09:31:25 +0300

    Fix Error in Class

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,6 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """Amenity Class"""
 
+    name = ""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = ""
--- models/review.py
+++ models/review.py
@@ -4,11 +4,11 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """Review Class"""
 
+    place_id = ""
+    user_id = ""
+    text = ""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
 
     def __str__(self):
         return "[{}] ({}) <{}>".format(self.__class__.__name__, self.id, self.__dict__)
