b8ae434f43460b3dc5617ace6ccbfb0b0d279de5
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-10-20 15:15:01 +0300

    Add update to default command

--- console.py
+++ console.py
@@ -164,7 +164,8 @@ class HBNBCommand(cmd.Cmd):
     
     def default(self, line):
         my_command_list= {"all(": self.do_all, "count(": self.do_count
-                          , "show(": self.do_show, "destroy(":self.do_destroy}
+                          , "show(": self.do_show, "destroy(":self.do_destroy
+                          , "update(": self.do_update}
 
         # handle input
         l = line.split(".")
